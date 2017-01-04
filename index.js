3611676cc07e7063228786453f5125b0129e6e42
an nguyen <hello.an.ng@gmail.com>
2017-01-04 11:32:05 -0800

    solves random pairing

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var fs = require("fs");
 // });
 // trash or err works the same (placeholder)
 
+// *** NAME PICKER! ***
+
 var data = fs.readFile('./students.txt', 'utf-8', function(err, data){
   if (err) throw err;
   // console.log(data);
@@ -24,4 +26,46 @@ var data = fs.readFile('./students.txt', 'utf-8', function(err, data){
   var name = names[randomIndex];
   console.log(name);
 });
+
+// *** RANDOM PAIRS ***
+
+var data = fs.readFile('./students.txt', 'utf-8', function(err, data){
+  if (err) throw err;
+
+  // turn string into array of names
+  var names = data.split('\n');
+  // removes empty ''
+  names.pop();
+  var groups = [];
+  while (names.length) {
+    var group = names.splice(0,2);
+    groups.push(group);
+  }
+  var lastGroup = groups.pop();
+  if (lastGroup.length != 2) {
+    var odd = lastGroup.pop();
+    lastGroup = groups.pop();
+    lastGroup.push(odd);
+    groups.push(lastGroup);
+  }
+  console.log(groups);
+});
+
+//   for (var i = 0; i < names.length; i++) {
+//     // picks random name from array
+//     var randomIndex = Math.floor( Math.random() * names.length);
+//     var nameOne = names[randomIndex];
+//     // remove name1 from array
+//     names.pop(nameOne);
+
+//     // pick a second random name
+//     var randomIndexTwo = Math.floor( Math.random() * names.length);
+//     var nameTwo = names[randomIndexTwo];
+//     names.pop(nameTwo);
+
+//     console.log(nameOne + " & " + nameTwo);
+//   }
+
+// });
+
 
